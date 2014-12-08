9ef87fc2b861aad3ef3dd1a4bf57f60ead7af67c
WangHaoRan <whrlmc@139.com>
2014-12-09 02:29:08 +0800

    123123

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -73,8 +73,39 @@ class Welcome extends CI_Controller {
 
     //朋友点击
     public function friend(){
+        //获取UID
         $uid = $this->input->get('uid');
-        var_dump($uid);
+        //授权跳转
+        $this->load->helper('url');
+        if(empty($_GET['code'])){
+            $token_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa06d1604e750da71&redirect_uri=' . urlencode('http://elle.cnhtk.cn/friend') . '&response_type=code&scope=snsapi_userinfo&state=' . $uid . '#wechat_redirect';
+            redirect($token_url);
+        }
+
+        //获取UID
+
+        $state_uid = $_GET['state'];
+
+        //拉取token
+        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxa06d1604e750da71&secret=9a1454d6c2503d961e976fc12c1e8dd8&code=' . $_GET['code'] . '&grant_type=authorization_code';
+        $result_json = file_get_contents($token_url);
+        $result_arr = json_decode($result_json, true);
+        if(!empty($result_arr['errcode'])){
+            die('<h1>Authorization failure1!' .  $result_arr['errmsg'] . '</h1>');
+        }
+
+        //获取信息
+        $info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $result_arr['access_token'] . '&openid=' . $result_arr['openid'] . '&lang=zh_CN';
+
+        $result_json = file_get_contents($info_url);
+        $result_arr = json_decode($result_json, true);
+        if(!empty($result_arr['errcode'])){
+            die('<h1>Authorization failure2!' .  $result_arr['errmsg'] . '</h1>');
+        }
+
+        var_dump($state_uid);
+        var_dump($result_arr);
+
     }
 
     public function testtttt(){
