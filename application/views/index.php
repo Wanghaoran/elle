05a63dea7f6ff378ade65dc6579fe03d63bacdf3
WangHaoRan <whrlmc@139.com>
2014-12-10 16:52:50 +0800

    123123

--- application/views/index.php
+++ application/views/index.php
@@ -19,7 +19,7 @@
                 snowflakesize: 2, //snowflakesize is 1 then it get the size of the image as random , if the snowflakesize is 2 means size of the image as custom
                 snowflakedirection: 1, // 1 means default no wind (top to bottom), 2 means random, 3 means left to right and 4 means  right to left
                 snownumberofflakes: 6, // number of flakes is user option
-                snowflakespeed: 6, // falling speed of flake 10 sec is default
+                snowflakespeed: 20, // falling speed of flake 10 sec is default
                 flakeheightandwidth: 30 // if you are mention that option flakesize is 2 then this flakeheightandwidth should work values are in pixels 16*16.
             });
         });
